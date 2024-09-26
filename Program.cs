dabdc70770eab913b73d04dc6fda6450ae376f41
hotbloxer <p-hougaard@hotmail.com>
2024-09-26 10:17:53 +0200

    jeg har tilføjet en funktion i main der kalder min klasses funktion see.mee

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@
             int b = 2;
             int c = a * b;
             Console.WriteLine("Hello, World! " + a + " * " + b + " = " + c);
+
+            Peters_Class peters = new Peters_Class();
+            peters.see_mee();
         }
     }
 }
