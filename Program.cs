acb74d47b547260e8db4fcee4cb244298a293afa
Lunte <Line.mh@hotmail.com>
2024-09-26 10:27:00 +0200

    commmiiiit

--- Program.cs
+++ Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-            int a = 1;  
+            int a = 3;  
             int b = 2;
             int c = a * b;
+           
+            
             Console.WriteLine("Hello, World! " + a + " * " + b + " = " + c);
 
             Peters_Class peters = new Peters_Class();
             peters.see_mee();
+
+           Line line = new Line();
         }
     }
 }
