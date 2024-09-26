39ec06cd05d8b2c234ea7153480e90b3b33840e7
Nicolaj Glud <nhglud@gmail.com>
2024-09-26 10:17:32 +0200

    changed int to long

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
             int a = 1;  
             int b = 2;
             int c = a * b;
-            int bigNumber1 = 7864188;
-            int bigNumber2 = 4851184;
-            int multBigNumbers = bigNumber1 * bigNumber2;
+            long bigNumber1 = 7864188;
+            long bigNumber2 = 4851184;
+            long multBigNumbers = bigNumber1 * bigNumber2;
             Console.WriteLine("Hello, World! " + a + " * " + b + " = " + c);
             Console.WriteLine("Hold on to your hats! Multiplying BIIIG NUMBERS!!!: " + multBigNumbers);
         }
