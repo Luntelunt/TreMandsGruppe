27c85d81c334dfffdf2f40400a18bb6b246fb3ba
Lunte <Line.mh@hotmail.com>
2024-09-26 09:49:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TreMandsGruppe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
