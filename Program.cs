5f7df0ec08f578c3d7707cf1a372668b9d2412ed
Nicolaj Glud <nhglud@gmail.com>
2024-09-26 10:13:51 +0200

    Multiplyin some big numbers.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@
             int a = 1;  
             int b = 2;
             int c = a * b;
+            int bigNumber1 = 7864188;
+            int bigNumber2 = 4851184;
+            int multBigNumbers = bigNumber1 * bigNumber2;
             Console.WriteLine("Hello, World! " + a + " * " + b + " = " + c);
+            Console.WriteLine("Hold on to your hats! Multiplying BIIIG NUMBERS!!!: " + multBigNumbers);
         }
     }
 }
